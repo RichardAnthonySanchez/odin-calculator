36a60d535f1e4de6acbd54f3d125bab4323a41f7
richard <theirsaviorhd@gmail.com>
2022-10-27 07:41:49 -0400

    Calculator Works
    store the first number, the second number and operator
    equal key evaluates stored inputs
    populate the display with the result

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function addNumbers(x,y){
-    return x + y;
+    resultToDom(x + y);
 }
 
 function subtractNumbers(x,y){
-    return x - y;
+    resultToDom(x - y);
 }
 
 function multiplyNumbers(x,y){
-    return x * y;
+    resultToDom(x * y);
 }
 
 function divideNumbers(x,y){
-    return x / y;
+    resultToDom(x / y);
 }
 
 function operate(operator, x, y){
-    if (operator === "+"){
+    if (operator === '+'){
         return addNumbers(x,y);
-    } else if (operator === "-"){
+    } else if (operator === '-'){
         return subtractNumbers(x,y);
-    } else if (operator === "*"){
+    } else if (operator === '*'){
         return multiplyNumbers(x,y);
-    } else if (operator === "/"){
+    } else if (operator === '/'){
         return divideNumbers(x,y);
     } else {
         return null;
@@ -56,15 +56,62 @@ function buttonPress(x){
     } else {
         buttonToDom(null);
     }
-
 }
+
+//operation button gets interpreted
+function operationClick(x){
+    if (x === 'plus'){
+        operationToDom('+');
+    } else if (x === 'minus'){
+        operationToDom('-');
+    } else if (x === 'multiply'){
+        operationToDom('*');
+    } else if (x === 'divide'){
+        operationToDom('/');
+    } else {
+        operationToDom(null);
+    }
+}
+
+//operator button click
+const operatorClick = document.querySelectorAll('.operator-btn')
+operatorClick.forEach((opBtn) => {
+    opBtn.addEventListener('click', () => {
+        const userOperation = operationClick(opBtn.classList[0]);
+    });
+});
+
+//send final result to the dom
+function resultToDom(x){
+    const display = document.querySelector('.display-content');
+    display.textContent = x;
+    display.append();
+}
+
+//send operation to the dom
+function operationToDom(x){
+    const display = document.querySelector('.display-content');
+    display.textContent += x;
+    display.append();
+    operationArr.push(x);
+}
+
+//display value
+const displayArr = [];
+const operationArr = [];
+const displayArrTwo = [];
+
 
 //send button's result to the dom
 function buttonToDom(x){
     const display = document.querySelector('.display-content');
-    display.textContent = x;
+    display.textContent += x;
     display.append();
-
+    if (operationArr[operationArr.length - 1] === undefined){
+        displayArr.push(display.textContent);
+    } else {
+        displayArrTwo.push(x); 
+    }
 }
 
 //when a button is clicked send input to be interpreted
@@ -72,5 +119,13 @@ const digits = document.querySelectorAll('.digit-btn')
 digits.forEach((digitBtn) => {
     digitBtn.addEventListener('click', () => {
         buttonPress(digitBtn.classList[0]);
+    });
+});
+
+//when the equal button is clicked send inputs to operate
+const equals = document.querySelectorAll('.equals')
+equals.forEach((equalsBtn) => {
+    equalsBtn.addEventListener('click', () => {
+        operate(operationArr[operationArr.length - 1], displayArr[displayArr.length -1], displayArrTwo[displayArrTwo.length -1]);
     });
 });
