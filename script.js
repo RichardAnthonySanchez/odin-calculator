4c702232577d753142dc82100f94869609fdd091
richard <theirsaviorhd@gmail.com>
2022-10-27 06:16:32 -0400

    Numbers Display When User Clicks Buttons
    button clicks send information
    information is interpreted
    interpreted result is sent to the display so the user can see their result

--- script.js
+++ script.js
@@ -27,3 +27,50 @@ function operate(operator, x, y){
         return null;
     }
 }
+
+//finds the button that was pressed
+function buttonPress(x){
+    console.log("your button is " + x)
+    if (x === 'seven'){
+        buttonToDom(7);
+    } else if (x === 'eight'){
+        buttonToDom(8);
+    } else if (x === 'nine'){
+        buttonToDom(9);
+    } else if (x === 'four'){
+        buttonToDom(4);
+    } else if (x === 'five'){
+        buttonToDom(5);
+    } else if (x === 'six'){
+        buttonToDom(6);
+    } else if (x === 'one'){
+        buttonToDom(1);
+    } else if (x === 'two'){
+        buttonToDom(2);
+    } else if (x === 'three'){
+        buttonToDom(3);
+    } else if (x === 'zero'){
+        buttonToDom(0);
+    } else if (x === 'decimal'){
+        buttonToDom('.');
+    } else {
+        buttonToDom(null);
+    }
+
+}
+
+//send button's result to the dom
+function buttonToDom(x){
+    const display = document.querySelector('.display-content');
+    display.textContent = x;
+    display.append();
+
+}
+
+//when a button is clicked send input to be interpreted
+const digits = document.querySelectorAll('.digit-btn')
+digits.forEach((digitBtn) => {
+    digitBtn.addEventListener('click', () => {
+        buttonPress(digitBtn.classList[0]);
+    });
+});
